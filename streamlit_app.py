ee654627fce96add5e1596117030826ac2395608
padmini-naik <123750414+padmini-naik@users.noreply.github.com>
2023-01-28 18:29:05 +0530

    Update streamlit_app.py
    Added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit;
 streamlit.title('Helowwww');
 streamlit.header('Header');
-streamlit.text(' Text1');
+streamlit.text('+. Text1');
 streamlit.text('Text2');
 streamlit.text('Text3');
+
+import pandas;
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+streamlit.dataframe(my_fruit_list);
