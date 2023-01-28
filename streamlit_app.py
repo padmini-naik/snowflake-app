02fbeb5d1c6d579870b87cd61013511fe26042c9
padmini-naik <123750414+padmini-naik@users.noreply.github.com>
2023-01-28 18:18:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit;
-streamlit.title("Helowwww");
-streamlit.header("Header");
-streamlit.text("Text1");
-streamlit.text("Text2");
-streamlit.text("Text3");
+streamlit.title('Helowwww');
+streamlit.header('Header');
+streamlit.text(' Text1');
+streamlit.text('Text2');
+streamlit.text('Text3');
